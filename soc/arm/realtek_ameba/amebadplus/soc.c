5bbb8fd5862f4a8f9dcf8c54f4d3cba7a73f1176
chunlin_yi <chunlin_yi@realsil.com.cn>
2024-06-12 12:41:30 +0800

    [amebadplus][zephyr]call ipc_table_init for IPC init +run IPC init
    Change Note:
    IPC should do init before main function start,
    set it as APPLICATION level
    
    CHIP:amebesmart
    
    Release Note:
    -author: chunlin_yi@realsil.com.cn
    -reviewer: neo_liu@realsil.com.cn
    
    test:
    1. compile&test success
    
    Change-Id: I29d891d2bb3664d6a922dbbba97f54e7aed6a49d

--- soc/arm/realtek_ameba/amebadplus/soc.c
+++ soc/arm/realtek_ameba/amebadplus/soc.c
@@ -4,10 +4,11 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <ameba_soc.h>
+#include <soc.h>
 #include <zephyr/device.h>
 #include <zephyr/init.h>
-#include <soc.h>
-#include "ameba_soc.h"
+#include <zephyr/kernel.h>
 
 
 #define SECTION(_name)      __attribute__ ((__section__(_name)))
@@ -24,6 +25,19 @@ RAM_START_FUNCTION Img2EntryFun0 = {
 	NULL,//BOOT_RAM_WakeFromPG,
 	(uint32_t)NewVectorTable
 };
+
+
+static int amebadplus_app_init(void)
+{
+	/* Register IPC interrupt */
+	IRQ_CONNECT(IPC_KM4_IRQ, INT_PRI5, IPC_INTHandler, (uint32_t)IPCKM4_DEV, 0);
+	irq_enable(IPC_KM4_IRQ);
+
+	/* IPC table initialization */
+	ipc_table_init(IPCKM4_DEV);
+
+	return 0;
+}
 
 static int amebadplus_init(void)
 {
@@ -47,3 +61,4 @@ static int amebadplus_init(void)
 }
 
 SYS_INIT(amebadplus_init, PRE_KERNEL_1, 0);
+SYS_INIT(amebadplus_app_init, APPLICATION, 0);
