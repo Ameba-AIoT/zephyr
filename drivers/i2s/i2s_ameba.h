69a3cd10de93e1c82be3270e380fbae7ef817675
melinda_cui <melinda_cui@realsil.com.cn>
2024-08-06 14:36:10 +0800

    [amebadplus][zephyr] update i2s and dmic driver
    * update i2s driver
    * update dmic driver
    
    Change-Id: Ic010d3ed5efa4f7fa9f4b4e1efec9e22fa925bf6

--- drivers/i2s/i2s_ameba.h
+++ drivers/i2s/i2s_ameba.h
@@ -13,35 +13,17 @@ struct i2s_ameba_cfg {
 	const struct device *clock_dev;
 	clock_control_subsys_t clock_subsys;
 	const struct pinctrl_dev_config *pcfg;
+	bool MultiIO;
+	bool mono_stereo;
 	uint8_t index;
-	uint32_t mclk_multiple;
-	uint32_t mclk_fixed_max;
 	uint8_t tdmmode;
 	uint8_t fifo_num;
-	bool MultiIO;
-	uint16_t chn_len;
-	bool mono_stereo;
-	uint32_t clock_mode;
 	uint8_t pll_tune;
+	uint16_t chn_len;
+	uint32_t mclk_multiple;
+	uint32_t mclk_fixed_max;
+	uint32_t clock_mode;
 };
-
-struct AUDIO_ClockParams {
-	u32 Clock;
-	u32 MCLK_NI;
-	u32 MCLK_MI;
-};
-
-struct AUDIO_InitParams {
-	u32 sr;
-	u32 chn_len;
-	u32 chn_cnt;
-	u32 codec_multiplier_with_rate;
-	u32 sport_mclk_fixed_max;
-};
-
-#define PLL_CLOCK_45P1584M	45158400
-#define PLL_CLOCK_98P304M	98304000
-#define I2S_CLOCK_XTAL40M	40000000
 
 struct SP_GDMA_STRUCT {
 	GDMA_InitTypeDef       	SpTxGdmaInitStruct;
@@ -50,17 +32,24 @@ struct SP_GDMA_STRUCT {
 	GDMA_InitTypeDef       	SpRxGdmaInitStructExt;
 };
 
-struct SP_GDMA_STRUCT audio_global;
-
 struct stream {
 	int32_t state;
 	struct i2s_config cfg;
+	uint8_t free_tx_dma_blocks;
+	bool last_block;
+	struct k_msgq in_queue;
+	struct k_msgq out_queue;
+
 };
 
 /* Device run time data */
 struct i2s_ameba_data {
 	struct stream tx;
 	struct stream rx;
+	void *tx_in_msgs[CONFIG_I2S_TX_BLOCK_COUNT];
+	void *tx_out_msgs[CONFIG_I2S_TX_BLOCK_COUNT];
+	void *rx_in_msgs[CONFIG_I2S_RX_BLOCK_COUNT];
+	void *rx_out_msgs[CONFIG_I2S_RX_BLOCK_COUNT];
+	struct SP_GDMA_STRUCT sp;
 };
-
 #endif	/* _ameba_I2S_H_ */
