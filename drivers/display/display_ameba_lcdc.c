7b4ddc06b11c0c2ba10f89ede6e134b21a7fe224
zjian zhang <zjian_zhang@realsil.com.cn>
2025-09-02 13:57:48 +0800

    [amebaG2][zephyr] update lcdc driver
    * update lcdc driver
    
    Change-Id: I47886a41a754b451f2a97e7ebff5effc415fd241

---
+++ drivers/display/display_ameba_lcdc.c
@@ -0,0 +1,356 @@
+/*
+ * Copyright (c) 2025 Realtek Semiconductor Corp.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define DT_DRV_COMPAT realtek_ameba_lcdc
+
+#include <zephyr/drivers/display.h>
+#include <zephyr/drivers/pinctrl.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/kernel.h>
+#include <zephyr/cache.h>
+
+#include <zephyr/logging/log.h>
+#include <zephyr/irq.h>
+
+LOG_MODULE_REGISTER(display_ameba_lcdc, CONFIG_DISPLAY_LOG_LEVEL);
+
+#if CONFIG_AMEBA_LCDC_ARGB8888
+#define AMEBA_LCDC_INIT_PIXEL_SIZE   4u
+#define AMEBA_LCDC_INIT_PIXEL_FORMAT LCDC_PIXEL_FORMAT_ARGB8888
+#define DISPLAY_INIT_PIXEL_FORMAT    PIXEL_FORMAT_ARGB_8888
+#elif CONFIG_AMEBA_LCDC_RGB888
+#define AMEBA_LCDC_INIT_PIXEL_SIZE   3u
+#define AMEBA_LCDC_INIT_PIXEL_FORMAT LCDC_PIXEL_FORMAT_RGB888
+#define DISPLAY_INIT_PIXEL_FORMAT    PIXEL_FORMAT_RGB_888
+#elif CONFIG_AMEBA_LCDC_RGB565
+#define AMEBA_LCDC_INIT_PIXEL_SIZE   2u
+#define AMEBA_LCDC_INIT_PIXEL_FORMAT LCDC_PIXEL_FORMAT_RGB565
+#define DISPLAY_INIT_PIXEL_FORMAT    PIXEL_FORMAT_RGB_565
+#else
+#error "Invalid LCDC pixel format chosen"
+#endif
+
+#define AMEBA_LCDC_FRAME_BUFFER_LEN(inst)                                                          \
+	(AMEBA_LCDC_INIT_PIXEL_SIZE * DT_INST_PROP(inst, height) * DT_INST_PROP(inst, width))
+
+/* Define the heap size. 512 bytes of padding are included for kernel heap structures */
+K_HEAP_DEFINE(display_heap, AMEBA_LCDC_FRAME_BUFFER_LEN(0) * CONFIG_AMEBA_LCDC_FB_NUM + 512);
+
+/*static const uint32_t supported_fmts = PIXEL_FORMAT_BGR_565 | PIXEL_FORMAT_ARGB_8888;*/
+
+struct ameba_lcdc_config {
+	/*LCDIF_Type *base;*/
+	void (*irq_config_func)(const struct device *dev);
+	/*elcdif_rgb_mode_config_t rgb_mode;*/
+	const struct pinctrl_dev_config *pincfg;
+	const struct gpio_dt_spec backlight_gpio;
+};
+
+struct ameba_lcdc_data {
+	/* Pointer to active framebuffer */
+	const uint8_t *active_fb;
+	/* Pointers to driver allocated framebuffers */
+	uint8_t *fb[CONFIG_AMEBA_LCDC_FB_NUM];
+	enum display_pixel_format pixel_format;
+	size_t pixel_bytes;
+	size_t fb_bytes;
+	/*elcdif_rgb_mode_config_t rgb_mode;*/
+	struct k_sem sem;
+	/* Tracks index of next active driver framebuffer */
+	uint8_t next_idx;
+};
+
+#define AMEBA_LCDC_TODO 0
+
+static int ameba_lcdc_write(const struct device *dev, const uint16_t x, const uint16_t y,
+			    const struct display_buffer_descriptor *desc, const void *buf)
+{
+#if AMEBA_LCDC_TODO
+	const struct ameba_lcdc_config *config = dev->config;
+	struct ameba_lcdc_data *dev_data = dev->data;
+	int h_idx;
+	const uint8_t *src;
+	uint8_t *dst;
+	int ret = 0;
+	bool full_fb = false;
+
+	__ASSERT((dev_data->pixel_bytes * desc->pitch * desc->height) <= desc->buf_size,
+		 "Input buffer too small");
+
+	LOG_DBG("W=%d, H=%d, @%d,%d", desc->width, desc->height, x, y);
+
+	if ((x == 0) && (y == 0) && (desc->width == config->rgb_mode.panelWidth) &&
+	    (desc->height == config->rgb_mode.panelHeight) && (desc->pitch == desc->width)) {
+		/* We can use the display buffer directly, no need to copy it */
+		LOG_DBG("Setting FB from %p->%p", (void *)dev_data->active_fb, (void *)buf);
+		dev_data->active_fb = buf;
+		full_fb = true;
+	} else if ((x == 0) && (y == 0) && (desc->width == config->rgb_mode.panelHeight) &&
+		   (desc->height == config->rgb_mode.panelWidth) && (desc->pitch == desc->width) &&
+		   IS_ENABLED(CONFIG_AMEBA_LCDC_PXP)) {
+		/* With the PXP, we can rotate this display buffer to align
+		 * with output dimensions
+		 */
+		LOG_DBG("Setting FB from %p->%p", (void *)dev_data->active_fb, (void *)buf);
+		dev_data->active_fb = buf;
+		full_fb = true;
+	} else {
+		/* We must use partial framebuffer copy */
+		if (CONFIG_AMEBA_LCDC_FB_NUM == 0) {
+			LOG_ERR("Partial display refresh requires driver framebuffers");
+			return -ENOTSUP;
+		} else if (dev_data->active_fb != dev_data->fb[dev_data->next_idx]) {
+			/*
+			 * We must copy the entire current framebuffer to new
+			 * buffer, since we wil change the active buffer
+			 * address
+			 */
+			src = dev_data->active_fb;
+			dst = dev_data->fb[dev_data->next_idx];
+			memcpy(dst, src, dev_data->fb_bytes);
+		}
+		/* Now, write the display update into active framebuffer */
+		src = buf;
+		dst = dev_data->fb[dev_data->next_idx];
+		dst += dev_data->pixel_bytes * (y * config->rgb_mode.panelWidth + x);
+
+		for (h_idx = 0; h_idx < desc->height; h_idx++) {
+			memcpy(dst, src, dev_data->pixel_bytes * desc->width);
+			src += dev_data->pixel_bytes * desc->pitch;
+			dst += dev_data->pixel_bytes * config->rgb_mode.panelWidth;
+		}
+
+		LOG_DBG("Setting FB from %p->%p", (void *)dev_data->active_fb,
+			(void *)dev_data->fb[dev_data->next_idx]);
+		/* Set new active framebuffer */
+		dev_data->active_fb = dev_data->fb[dev_data->next_idx];
+	}
+
+#ifdef CONFIG_HAS_MCUX_CACHE
+	DCACHE_CleanByRange((uint32_t)dev_data->active_fb, dev_data->fb_bytes);
+#endif
+
+	/* Queue next framebuffer */
+	ELCDIF_SetNextBufferAddr(config->base, (uint32_t)dev_data->active_fb);
+
+#if CONFIG_AMEBA_LCDC_FB_NUM != 0
+	/* Update index of active framebuffer */
+	dev_data->next_idx = (dev_data->next_idx + 1) % CONFIG_AMEBA_LCDC_FB_NUM;
+#endif
+	/* Enable frame buffer completion interrupt */
+	ELCDIF_EnableInterrupts(config->base, kELCDIF_CurFrameDoneInterruptEnable);
+	/* Wait for frame send to complete */
+	k_sem_take(&dev_data->sem, K_FOREVER);
+	return ret;
+#endif
+	return 0;
+}
+
+static int ameba_lcdc_display_blanking_off(const struct device *dev)
+{
+	const struct ameba_lcdc_config *config = dev->config;
+
+	return gpio_pin_set_dt(&config->backlight_gpio, 1);
+}
+
+static int ameba_lcdc_display_blanking_on(const struct device *dev)
+{
+	const struct ameba_lcdc_config *config = dev->config;
+
+	return gpio_pin_set_dt(&config->backlight_gpio, 0);
+}
+
+static int ameba_lcdc_set_pixel_format(const struct device *dev,
+				       const enum display_pixel_format pixel_format)
+{
+#if AMEBA_LCDC_TODO
+	struct ameba_lcdc_data *dev_data = dev->data;
+	const struct ameba_lcdc_config *config = dev->config;
+
+	if (!(pixel_format & supported_fmts)) {
+		LOG_ERR("Unsupported pixel format");
+		return -ENOTSUP;
+	}
+
+	dev_data->pixel_format = pixel_format;
+	dev_data->pixel_bytes = DISPLAY_BITS_PER_PIXEL(pixel_format) / BITS_PER_BYTE;
+	dev_data->fb_bytes =
+		config->rgb_mode.panelWidth * config->rgb_mode.panelHeight * dev_data->pixel_bytes;
+
+	for (int i = 0; i < CONFIG_AMEBA_LCDC_FB_NUM; i++) {
+		k_heap_free(&display_heap, dev_data->fb[i]);
+		dev_data->fb[i] =
+			k_heap_aligned_alloc(&display_heap, 64, dev_data->fb_bytes, K_FOREVER);
+		if (dev_data->fb[i] == NULL) {
+			LOG_ERR("Could not allocate memory for framebuffers");
+			return -ENOMEM;
+		}
+		memset(dev_data->fb[i], 0, dev_data->fb_bytes);
+	}
+
+	dev_data->rgb_mode = config->rgb_mode;
+	if (pixel_format == PIXEL_FORMAT_BGR_565) {
+		dev_data->rgb_mode.pixelFormat = kELCDIF_PixelFormatRGB565;
+	} else if (pixel_format == PIXEL_FORMAT_RGB_888) {
+		dev_data->rgb_mode.pixelFormat = kELCDIF_PixelFormatRGB888;
+	} else if (pixel_format == PIXEL_FORMAT_ARGB_8888) {
+		dev_data->rgb_mode.pixelFormat = kELCDIF_PixelFormatXRGB8888;
+	}
+
+	ELCDIF_RgbModeSetPixelFormat(config->base, dev_data->rgb_mode.pixelFormat);
+#endif
+	return 0;
+}
+
+static int ameba_lcdc_set_orientation(const struct device *dev,
+				      const enum display_orientation orientation)
+{
+	if (orientation == DISPLAY_ORIENTATION_NORMAL) {
+		return 0;
+	}
+	LOG_ERR("Changing display orientation not implemented");
+	return -ENOTSUP;
+}
+
+static void ameba_lcdc_get_capabilities(const struct device *dev,
+					struct display_capabilities *capabilities)
+{
+#if AMEBA_LCDC_TODO
+	const struct ameba_lcdc_config *config = dev->config;
+
+	memset(capabilities, 0, sizeof(struct display_capabilities));
+	capabilities->x_resolution = config->rgb_mode.panelWidth;
+	capabilities->y_resolution = config->rgb_mode.panelHeight;
+	capabilities->supported_pixel_formats = supported_fmts;
+	capabilities->current_pixel_format = ((struct ameba_lcdc_data *)dev->data)->pixel_format;
+	capabilities->current_orientation = DISPLAY_ORIENTATION_NORMAL;
+#endif
+}
+
+static void ameba_lcdc_isr(const struct device *dev)
+{
+#if AMEBA_LCDC_TODO
+	const struct ameba_lcdc_config *config = dev->config;
+	struct ameba_lcdc_data *dev_data = dev->data;
+	uint32_t status;
+
+	status = ELCDIF_GetInterruptStatus(config->base);
+	ELCDIF_ClearInterruptStatus(config->base, status);
+	if (config->base->CUR_BUF == ((uint32_t)dev_data->active_fb)) {
+		/* Disable frame completion interrupt, post to
+		 * sem to notify that frame send is complete.
+		 */
+		ELCDIF_DisableInterrupts(config->base, kELCDIF_CurFrameDoneInterruptEnable);
+		k_sem_give(&dev_data->sem);
+	}
+#endif
+}
+
+static int ameba_lcdc_init(const struct device *dev)
+{
+#if AMEBA_LCDC_TODO
+	const struct ameba_lcdc_config *config = dev->config;
+	struct ameba_lcdc_data *dev_data = dev->data;
+	int err;
+
+	err = pinctrl_apply_state(config->pincfg, PINCTRL_STATE_DEFAULT);
+	if (err) {
+		return err;
+	}
+
+	err = gpio_pin_configure_dt(&config->backlight_gpio, GPIO_OUTPUT_ACTIVE);
+	if (err) {
+		return err;
+	}
+
+	k_sem_init(&dev_data->sem, 0, 1);
+
+	config->irq_config_func(dev);
+
+	/* Set default pixel format obtained from device tree */
+	ameba_lcdc_set_pixel_format(dev, dev_data->pixel_format);
+
+	dev_data->active_fb = dev_data->fb[0];
+
+	ELCDIF_RgbModeInit(config->base, &dev_data->rgb_mode);
+	ELCDIF_RgbModeStart(config->base);
+#endif
+	return 0;
+}
+
+static DEVICE_API(display, ameba_lcdc_api) = {
+	.blanking_on = ameba_lcdc_display_blanking_on,
+	.blanking_off = ameba_lcdc_display_blanking_off,
+	.write = ameba_lcdc_write,
+	.get_capabilities = ameba_lcdc_get_capabilities,
+	.set_pixel_format = ameba_lcdc_set_pixel_format,
+	.set_orientation = ameba_lcdc_set_orientation,
+};
+
+#if AMEBA_LCDC_TODO
+/*.rgb_mode =*/
+{
+	.panelWidth = DT_INST_PROP(id, width),
+	.panelHeight = DT_INST_PROP(id, height),
+	.hsw = DT_PROP(DT_INST_CHILD(id, display_timings), hsync_len),
+	.hfp = DT_PROP(DT_INST_CHILD(id, display_timings), hfront_porch),
+	.hbp = DT_PROP(DT_INST_CHILD(id, display_timings), hback_porch),
+	.vsw = DT_PROP(DT_INST_CHILD(id, display_timings), vsync_len),
+	.vfp = DT_PROP(DT_INST_CHILD(id, display_timings), vfront_porch),
+	.vbp = DT_PROP(DT_INST_CHILD(id, display_timings), vback_porch),
+	.polarityFlags = (DT_PROP(DT_INST_CHILD(id,
+
+						display_timings),
+				  hsync_active)
+				  ? kELCDIF_HsyncActiveHigh
+				  : kELCDIF_HsyncActiveLow) |
+			 (DT_PROP(DT_INST_CHILD(id,
+
+						display_timings),
+				  vsync_active)
+				  ? kELCDIF_VsyncActiveHigh
+				  : kELCDIF_VsyncActiveLow) |
+			 (DT_PROP(DT_INST_CHILD(id,
+
+						display_timings),
+				  de_active)
+				  ? kELCDIF_DataEnableActiveHigh
+				  : kELCDIF_DataEnableActiveLow) |
+			 (DT_PROP(DT_INST_CHILD(id,
+
+						display_timings),
+				  pixelclk_active)
+				  ? kELCDIF_DriveDataOnRisingClkEdge
+				  : kELCDIF_DriveDataOnFallingClkEdge),
+	.dataBus = LCDIF_CTRL_LCD_DATABUS_WIDTH(DT_INST_ENUM_IDX(id, data_bus_width)),
+},
+
+/*.backlight_gpio = GPIO_DT_SPEC_INST_GET(id, backlight_gpios), */
+#endif
+
+#define AMEBA_LCDC_DEVICE_INIT(id)                                                                 \
+	PINCTRL_DT_INST_DEFINE(id);                                                                \
+	static void ameba_lcdc_config_func_##id(const struct device *dev);                         \
+	static const struct ameba_lcdc_config ameba_lcdc_config_##id = {                           \
+		/*.base = (LCDIF_Type *)DT_INST_REG_ADDR(id),*/                                    \
+		.irq_config_func = ameba_lcdc_config_func_##id,                                    \
+		.pincfg = PINCTRL_DT_INST_DEV_CONFIG_GET(id),                                      \
+	};                                                                                         \
+	static struct ameba_lcdc_data ameba_lcdc_data_##id = {                                     \
+		.next_idx = 0,                                                                     \
+		.pixel_format = DT_INST_PROP(id, pixel_format),                                    \
+	};                                                                                         \
+	DEVICE_DT_INST_DEFINE(id, &ameba_lcdc_init, NULL, &ameba_lcdc_data_##id,                   \
+			      &ameba_lcdc_config_##id, POST_KERNEL, CONFIG_DISPLAY_INIT_PRIORITY,  \
+			      &ameba_lcdc_api);                                                    \
+	static void ameba_lcdc_config_func_##id(const struct device *dev)                          \
+	{                                                                                          \
+		IRQ_CONNECT(DT_INST_IRQN(id), DT_INST_IRQ(id, priority), ameba_lcdc_isr,           \
+			    DEVICE_DT_INST_GET(id), 0);                                            \
+		irq_enable(DT_INST_IRQN(id));                                                      \
+	}
+
+	DT_INST_FOREACH_STATUS_OKAY(AMEBA_LCDC_DEVICE_INIT)
