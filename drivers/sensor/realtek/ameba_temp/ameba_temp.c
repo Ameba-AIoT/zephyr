46505b59c1078efc082bfd06ee80cac3a9c0f894
katherine_wang <katherine_wang@realsil.com.cn>
2025-09-16 17:51:47 +0800

    [zephyr][sensor] update thermal driver
    * update thermal driver on green2
    + add ameba overlay under tests and samples
    - remove useless alias from list
    
    DEPEND: Iab7cb1eec4f22151f4d1a
    Change-Id: If4124a1be15933975d05c8071c61e900dc4ef077

--- drivers/sensor/realtek/ameba_temp/ameba_temp.c
+++ drivers/sensor/realtek/ameba_temp/ameba_temp.c
@@ -18,32 +18,25 @@
 
 LOG_MODULE_REGISTER(ameba_temp, CONFIG_SENSOR_LOG_LEVEL);
 
-#define TEMP_TODO 0
-#ifdef TEMP_TODO
-typedef int temp_sensor_dac_offset_t;
-typedef int *temp_sensor_config_t;
-#endif
-
 struct ameba_temp_data {
 	struct k_mutex mutex;
-	temp_sensor_config_t temp_sensor;
-	float temp_out;
-};
-
-struct ameba_temp_config {
-	temp_sensor_dac_offset_t range;
+	float c_temp; /* Celsius degree of float type */
 };
 
 static int ameba_temp_sample_fetch(const struct device *dev, enum sensor_channel chan)
 {
-#if TEMP_TODO
-
+	(void)chan;
 	struct ameba_temp_data *data = dev->data;
+	u32 tm;
 	int rc = 0;
 
 	k_mutex_lock(&data->mutex, K_FOREVER);
 
-	if (temp_sensor_read_celsius(&data->temp_out) != ESP_OK) {
+	tm = TM_GetTempResult();
+
+	if (tm != TM_INVALID_VALUE) {
+		data->c_temp = TM_GetCdegree(tm);
+	} else {
 		LOG_ERR("Temperature read error!");
 		rc = -EFAULT;
 		goto unlock;
@@ -53,23 +46,19 @@ unlock:
 	k_mutex_unlock(&data->mutex);
 
 	return rc;
-#endif
-	return 0;
 }
 
 static int ameba_temp_channel_get(const struct device *dev, enum sensor_channel chan,
 				  struct sensor_value *val)
 {
-#if TEMP_TODO
 	struct ameba_temp_data *data = dev->data;
 
-	if (chan != SENSOR_CHAN_DIE_TEMP) {
+	if (chan != SENSOR_CHAN_ALL && chan != SENSOR_CHAN_DIE_TEMP &&
+	    chan != SENSOR_CHAN_AMBIENT_TEMP) {
 		return -ENOTSUP;
 	}
 
-	return sensor_value_from_double(val, data->temp_out);
-#endif
-	return 0;
+	return sensor_value_from_float(val, data->c_temp);
 }
 
 static DEVICE_API(sensor, ameba_temp_driver_api) = {
@@ -79,32 +68,29 @@ static DEVICE_API(sensor, ameba_temp_driver_api) = {
 
 static int ameba_temp_init(const struct device *dev)
 {
-#if TEMP_TODO
 	struct ameba_temp_data *data = dev->data;
-	const struct ameba_temp_config *conf = dev->config;
+	TM_InitTypeDef TM_InitStruct;
 
 	k_mutex_init(&data->mutex);
-	temp_sensor_get_config(&data->temp_sensor);
-	data->temp_sensor.dac_offset = conf->range;
-	temp_sensor_set_config(data->temp_sensor);
-	temp_sensor_start();
-	LOG_DBG("Temperature sensor started. Offset %d, clk_div %d", data->temp_sensor.dac_offset,
-		data->temp_sensor.clk_div);
-#endif
+
+	TM_StructInit(&TM_InitStruct);
+	TM_Init(&TM_InitStruct);
+
+	/* disable high_pt & high_wt & low_wt */
+	TM_INTConfig(TM_BIT_IMR_TM_HIGH_WT | TM_BIT_IMR_TM_LOW_WT, DISABLE);
+	TM_HighPtConfig(0x0, DISABLE);
+	TM_HighWtConfig(0x0, DISABLE);
+	TM_LowWtConfig(0x0, DISABLE);
+
+	LOG_DBG("Thermal init ok.");
+
 	return 0;
 }
 
 #define AMEBA_TEMP_DEFINE(inst)                                                                    \
-	static struct ameba_temp_data ameba_temp_dev_data_##inst = {                               \
-		.temp_sensor = NULL,                                                               \
-	};                                                                                         \
-                                                                                                   \
-	static const struct ameba_temp_config ameba_temp_dev_config_##inst = {                     \
-		.range = (temp_sensor_dac_offset_t)DT_INST_PROP(inst, range),                      \
-	};                                                                                         \
-                                                                                                   \
+	static struct ameba_temp_data ameba_temp_dev_data_##inst = {};                             \
 	SENSOR_DEVICE_DT_INST_DEFINE(inst, ameba_temp_init, NULL, &ameba_temp_dev_data_##inst,     \
-				     &ameba_temp_dev_config_##inst, POST_KERNEL,                   \
-				     CONFIG_SENSOR_INIT_PRIORITY, &ameba_temp_driver_api);
+				     NULL, POST_KERNEL, CONFIG_SENSOR_INIT_PRIORITY,               \
+				     &ameba_temp_driver_api);
 
 DT_INST_FOREACH_STATUS_OKAY(AMEBA_TEMP_DEFINE)
