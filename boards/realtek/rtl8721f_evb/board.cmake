9bdd3952ae216f7605fdd8e32bc30d2efcbc26a9
jiong_zhang <jiong_zhang@realsil.com.cn>
2025-12-15 17:55:50 +0800

    [zephyr] add flash/debug parameters
    Change-Id: Ie6055285f056df6381d4b404f93329a510b39fa6

--- boards/realtek/rtl8721f_evb/board.cmake
+++ boards/realtek/rtl8721f_evb/board.cmake
@@ -1,10 +1,17 @@
 # Copyright (c) 2024 Realtek Semiconductor Corp.
 # SPDX-License-Identifier: Apache-2.0
 
+dt_chosen(SHELLUART PROPERTY zephyr,shell-uart)
+if(SHELLUART)
+  dt_prop(SHELLUART_BAUDRATE PATH ${SHELLUART} PROPERTY current-speed)
+  board_runner_args(amebaflash "--baudrate=${SHELLUART_BAUDRATE}")
+endif()
+
 board_runner_args(amebaflash "--image-dir=${ZEPHYR_BINARY_DIR}/../images" "--device=${CONFIG_SOC_SERIES}")
 board_set_flasher_ifnset(amebaflash)
 board_finalize_runner_args(amebaflash)
 
 board_runner_args(jlink "--device=Cortex-M55" "--speed=4000")
+board_runner_args(jlink "--tool-opt=-scriptfile ${CMAKE_CURRENT_LIST_DIR}/support/AP2_AP.JLinkScript")
 
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
--- boards/realtek/rtl872xd_evb/board.cmake
+++ boards/realtek/rtl872xd_evb/board.cmake
@@ -2,5 +2,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
 board_runner_args(jlink "--device=Cortex-M33" "--speed=4000")
+board_runner_args(jlink "--tool-opt=-scriptfile ${CMAKE_CURRENT_LIST_DIR}/support/AP2_KM4.JLinkScript")
 
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
